5f41612267c0b9accbaf598c8ed5173b6b3bdc47
ihsuncloud <ihsun@tuta.io>
2024-08-17 00:36:45 +0330

    🚀 Add Color Palette.

---
+++ lib/src/kernel/constants/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class ColorPalette {
+  static const Color cynicalBlack = Color(0xFF171717);
+  static const Color lighterPurple = Color(0xFFA95EFA);
+  static const Color sweetScape = Color(0xFF8A49F7);
+}
