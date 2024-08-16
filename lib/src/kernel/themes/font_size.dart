d96c7b1b05d9e959fb40c0cc02358ba1ecbe99a4
ihsuncloud <ihsun@tuta.io>
2024-08-17 00:39:08 +0330

    🚀 Add font sizes.

---
+++ lib/src/kernel/themes/font_size.dart
@@ -0,0 +1,8 @@
+class FontSizes {
+  static const extraSmall = 14.0;
+  static const small = 16.0;
+  static const standard = 18.0;
+  static const large = 20.0;
+  static const extraLarge = 24.0;
+  static const doubleExtraLarge = 32.0;
+}
