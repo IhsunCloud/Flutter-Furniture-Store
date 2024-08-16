20b16c322d52931b74fd929d11ce182e5dcc58c3
ihsuncloud <ihsun@tuta.io>
2024-08-17 00:37:13 +0330

    🚀 Add Graphic Assets.

---
+++ lib/src/kernel/constants/images.dart
@@ -0,0 +1,40 @@
+class GraphicAssets {
+  // Image path.
+  static const String imagePath = 'assets/images';
+  // Icon path.
+  static const String iconPath = 'assets/icons/';
+
+  // ************************************************************************ //
+  // Asset Images.
+
+  // green armchair.
+  static const String greenArmchair = '$imagePath/green_armchair.png';
+  // ikea_chair
+  static const String ikeaChair = '$imagePath/ikea_chair.png';
+  // modern_armchair
+  static const String modernArmchair = '$imagePath/modern_armchair.png';
+  static const String modernArmchair2 = '$imagePath/modern_armchair2.png';
+  // sofa red.
+  static const String sofaRed = '$imagePath/sofa_red.png';
+  // wood frame.
+  static const woodFrame = '$imagePath/wood_frame.png';
+  // yellow armchair.
+  static const String yellowArmchair = '$imagePath/yellow_armchair.png';
+  static const String yellowArmchair3 = '$imagePath/yellow_armchair3.png';
+
+  // ************************************************************************ //
+  // Asset Icons.
+
+  // home icon.
+  static const String homeIcon = '$iconPath/home.png';
+  // profile icon.
+  static const String profileIcon = '$iconPath/profile.png';
+  // qr icon.
+  static const String qrIcon = '$iconPath/qr.png';
+  // search icon.
+  static const String searchIcon = '$iconPath/search.png';
+  // shopping bag icon.
+  static const String shoppingBagIcon = '$iconPath/shopping_bag.png';
+  // top bars icon.
+  static const String topBarIcon = '$iconPath/top_bars_icon.png';
+}
