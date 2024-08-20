d4d2598a3e1b09d6a0dc58aee753acfdf68799c7
ihsuncloud <ihsun@tuta.io>
2024-08-21 01:05:34 +0330

    🚀 Add text palette.

--- lib/src/kernel/constants/texts.dart
+++ lib/src/kernel/constants/texts.dart
@@ -1,4 +1,9 @@
 class TextPalette {
   static const String appTitle = 'Furniture Store';
-  static const String scan = 'Scan';
+  static const String scan = 'جستجو تصویر';
+  static const String browsByCategory = 'دسته بندی';
+  static const String recommended = 'پیشنهاد ویژه';
+
+  static const String productCount1 = 'بیش از ۱۰۰ محصول';
+  static const String productCount2 = 'بیش از ۶۰ محصول';
 }
