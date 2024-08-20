9c5e006256a349c56a233da88ded7708d5c378f0
ihsuncloud <ihsun@tuta.io>
2024-08-21 01:05:12 +0330

    🚀 Add graphic assets.

--- lib/src/kernel/constants/images.dart
+++ lib/src/kernel/constants/images.dart
@@ -8,25 +8,44 @@ class GraphicAssets {
   // Asset Images.
 
   // green armchair.
+  static const String luxuryKitchenChair =
+      '$imagePath/luxury-kitchen-chair.png';
   static const String greenArmchair = '$imagePath/green_armchair.png';
+  static const String armchairWithCushions =
+      '$imagePath/modern-armchair-with-cushions.png';
+  static const String modernBlueChair =
+      '$imagePath/modern-blue-lounge-chair.png';
   // ikea_chair
   static const String ikeaChair = '$imagePath/ikea_chair.png';
+  static const String ikeaChair5 = '$imagePath/pngwing-2.png';
   // modern_armchair
+  static const String floralChair =
+      '$imagePath/chair-with-a-floral-pattern.png';
   static const String modernArmchair = '$imagePath/modern_armchair.png';
   static const String modernArmchair2 = '$imagePath/modern_armchair2.png';
   // sofa red.
   static const String sofaRed = '$imagePath/sofa_red.png';
+  static const String innovativeSofa = '$imagePath/innovative-sofa-01.png';
   // wood frame.
   static const woodFrame = '$imagePath/wood_frame.png';
+  static const slipperChair = '$imagePath/slipper-chair.png';
+  // red baroque.
+  static const redBaroque = '$imagePath/red-baroque.png';
   // yellow armchair.
   static const String yellowArmchair = '$imagePath/yellow_armchair.png';
-  static const String yellowArmchair3 = '$imagePath/yellow_armchair3.png';
+  static const String yellowArmchair3 = '$imagePath/yellow_armchair_3.png';
+  static const String redLeatherChair1 = '$imagePath/red-leather-chair.png';
+  static const String redArmchair = '$imagePath/red_armchair.png';
+  static const String woodenChair22 = '$imagePath/wooden-chair-22.png';
+  static const String woodenChair23 = '$imagePath/wooden-chair-23.png';
 
   // ************************************************************************ //
   // Asset Icons.
 
   // home icon.
   static const String homeIcon = '$iconPath/home.png';
+  // logo icon.
+  static const String logo = '$iconPath/logo.png';
   // profile icon.
   static const String profileIcon = '$iconPath/profile.png';
   // qr icon.
