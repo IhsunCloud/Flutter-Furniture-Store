960061255fdb7b37b9ae7dbe200b17bc3e51fb28
ihsuncloud <ihsun@tuta.io>
2024-08-21 01:04:52 +0330

    🚀 Add color constants.

--- lib/src/kernel/constants/colors.dart
+++ lib/src/kernel/constants/colors.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 
 class ColorPalette {
-  static const Color cynicalBlack = Color(0xFF171717);
-  static const Color lighterPurple = Color(0xFFA95EFA);
-  static const Color sweetScape = Color(0xFF8A49F7);
+  static const Color butterflyBlue = Color(0xFF219ebc);
+  static const Color boatsWain = Color(0xFF223658);
+  static const Color cosmosBlue = Color(0xFF023047);
+  static const Color deluxeDays = Color(0xFF8ecae6);
+  static const Color lavenderBonnet = Color(0xFF9496c1);
+  static const Color lightGray = Color(0xFFF3F6F8);
+  static const Color seaMount = Color(0xFF15616d);
+  static const Color sunFlowerMango = Color(0xFFffb703);
 }
